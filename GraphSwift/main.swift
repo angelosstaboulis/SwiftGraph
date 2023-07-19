95a182a2e0e2d310f9dd8fdf9a8bb8ee4b01d541
Angelos Staboulis <angelosstaboulis@gmail.com>
2023-07-19 15:02:55 +0300

    Commit refactor code

--- GraphSwift/main.swift
+++ GraphSwift/main.swift
@@ -11,8 +11,6 @@ open class Node<T>: GKGraphNode {
 
     public let value: T
     
-    private var connectionWeight: [GKGraphNode: Float] = [:]
-    
     public init(value: T) {
         self.value = value
         super.init()
@@ -21,38 +19,15 @@ open class Node<T>: GKGraphNode {
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override public var description: String {
-        String(describing: value)
+    override public var description:String{
+        String(describing:value)
     }
-    
-    public override func cost(to node: GKGraphNode) -> Float {
-        connectionWeight[node] ?? (node == self ? 0 : Float.infinity)
-    }
-    
     public func addConnection(to node: GKGraphNode,
-                              bidirectional: Bool = false,
-                              weight: Float = 1) {
-        addConnections(to: [node], bidirectional: bidirectional, weight: weight)
+                              bidirectional: Bool = false) {
+        super.addConnections(to: [node], bidirectional: bidirectional)
     }
-    public func addConnections(to nodes: [GKGraphNode],
-                               bidirectional: Bool = false,
-                               weight: Float = 1) {
-        super.addConnections(to: nodes, bidirectional: bidirectional)
-        
-        for node in nodes {
-
-            if connectionWeight[node] == nil {
-                connectionWeight[node] = weight
-            }
-            
-            if
-                bidirectional,
-                let node = node as? Node,
-                node.connectionWeight[self] == nil {
-                node.connectionWeight[self] = weight
-            }
-        }
+    public func removeConnection(to node:GKGraphNode,bidirectional:Bool = false){
+        super.removeConnections(to: [node], bidirectional: bidirectional)
     }
 }
 
@@ -68,17 +43,17 @@ let nodeC = Node(value: "C")
 let nodeD = Node(value: "D")
 let nodeE = Node(value: "E")
 // Make connections.
-nodeA.addConnection(to: nodeB, bidirectional: true, weight: 1)
-nodeB.addConnection(to: nodeC, bidirectional: true, weight: 2)
-nodeC.addConnection(to: nodeB, bidirectional: true, weight: 3)
-nodeD.addConnection(to: nodeC, bidirectional: true, weight: 4)
-nodeE.addConnection(to: nodeA, bidirectional: true, weight: 5)
+nodeA.addConnection(to: nodeB, bidirectional: true)
+nodeB.addConnection(to: nodeC, bidirectional: true)
+nodeC.addConnection(to: nodeB, bidirectional: true)
+nodeD.addConnection(to: nodeC, bidirectional: true)
+nodeE.addConnection(to: nodeA, bidirectional: true)
 
 // Create graph.
 let graph = Graph<Any>([nodeA, nodeB, nodeC,nodeD,nodeE])
 
 // Find path.
-let paths = graph.findPath(from: nodeC, to: nodeA)
+let paths = graph.findPath(from: nodeD, to: nodeB)
 
 debugPrint("node=",paths)
 
